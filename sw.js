a0a32fdf0111bf9d42cab363aa03b76194b6511b
EmmanuelMhlanga <145326894+EmmanuelMhlanga@users.noreply.github.com>
2026-02-10 19:59:03 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'gasa-tech-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/about.html',
+  '/services.html',
+  '/portfolio.html',
+  '/pricing.html',
+  '/contact.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets from Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
